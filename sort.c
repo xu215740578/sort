855e1ac5b973e4571020e66ccaf09e71f0aebdd9
CentOS7 <qihan123@localhost.localdomain>
2019-01-21 15:03:52 +0800

    modified sort.c

--- sort.c
+++ sort.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-#define swap(a,b) {a=a^b;b=a^b;a=a^b;}
+#define swap(a,b) {int temp=a; a=b; b=temp;}
 
 
 /**
@@ -76,8 +76,6 @@ void selection_sort(int array[], int n)
 
 void double_selection_sort(int array[], int n)
 {
-	if(n <= 1)
-		return;
 	int i, j, minPos, maxPos, temp;
 	for (i = 0; i < n/2-1; ++i)
 	{
@@ -111,13 +109,86 @@ void double_selection_sort(int array[], int n)
 	}
 }
 
+/* 
+ *算法分析:    
+    time-complexity: 最好情况下时间复杂度为O(nlog2n),待排序列接近无序，算法效率越高。
+                     最坏情况下时间复杂度为O(n2),待排序列月接近有序，算法效率越低。
+                     平均情况下时间复杂度为O(nlog2n);
+    space-complexity O(log2n)
+
+ *原理:
+    通过一趟排序将要排序的数据分割成独立的两部分，其中一部分的所有数据都比另外一部分的所有数据都要小，
+    然后再按此方法对这两部分数据分别进行快速排序，整个排序过程可以递归进行，以此达到整个数据变成有序序列
+*/
+void quick_sort(int a[], int low, int high)
+{
+	int pivot;
+	int i = low, j = high;
+	if (low < high)
+	{
+		pivot = a[low];    //将第一个元素作为枢轴
+		while(i < j)
+		{
+			while(i < j && a[j] >= pivot) //从右往左找到第一个小于枢轴的数
+				j--;
+			if(i < j)
+			{ //将该数移动到左指针的位置
+				a[i] = a[j];
+				i++;
+			}
+			while(i < j && a[i] < pivot) //从左往右找到第一个大于等于枢轴的数
+				i++;
+			if(i < j)
+			{ //将该数移动到右指针的位置
+				a[j] = a[i];
+				j--;
+			}
+		}
+		a[i] = pivot; //循环退出时i == j即枢轴的位置
+		//递归处理当前枢轴的左右两部分
+        quick_sort(a, low, i-1);
+        quick_sort(a, i+1, high);
+	}
+}
+
+void qsort(int a[], int left, int right)
+{
+	int index = left;
+	int pivot = a[left];
+	int l=left,r=right;
+
+	while(l<r)
+	{
+		for(; l<r && a[r]>pivot; --r);
+		swap(a[r], a[index]);
+		index = r;
+		for(; l<r && a[l]<=pivot; ++l);
+		swap(a[l], a[index]);
+		index = l;
+	}
+	if(left < index-1)
+		qsort(a, left, index-1);
+	if(right > index+1)
+		qsort(a, index+1, right);
+}
+
 int main()
 {
 	int i;
 	int array[10] = {2,4,7,6,5,8,3,9,1,0};
+	printf("input array:\n");
+	for (i = 0; i < sizeof(array)/sizeof(int); ++i)
+	{
+		printf(" %d", array[i]);
+	}
+	printf("\n");
+
 	//bubble_sort(array, sizeof(array)/sizeof(int));
 	//insertion_sort(array, sizeof(array)/sizeof(int));
-	selection_sort(array, sizeof(array)/sizeof(int));
+	//selection_sort(array, sizeof(array)/sizeof(int));
+	//double_selection_sort(array, sizeof(array)/sizeof(int));
+	quick_sort(array, 0, 9);
+	printf("sorted array:\n");
 	for (i = 0; i < sizeof(array)/sizeof(int); ++i)
 	{
 		printf(" %d", array[i]);
